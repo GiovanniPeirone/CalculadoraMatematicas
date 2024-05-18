d0d4a26db26dc05f569d735bac3d49dbaab25db4
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-05-17 23:57:38 -0300

    a

--- CalculadoraMatematicas/CalculadoraMatematicas/Program.cs
+++ CalculadoraMatematicas/CalculadoraMatematicas/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using CalculadoraMatematicas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace main
 {
     class Peogram
     {
+        static void menu()
+        {
+            string menu = """
+                1-Funcion Lineal
+                2-Salir
+                """;
+            Console.WriteLine(menu);
+        }
+
+
         static void Main(string[] args) 
         {
-            Console.WriteLine("0");
+            menu();
+
+           
+
+            Suma suma = new Suma(1,1);
+
+            suma.sumar();
+     
+
+            Console.WriteLine("END 0");
         }
     }
 }
---
+++ CalculadoraMatematicas/CalculadoraMatematicas/Suma.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraMatematicas
+{
+    internal class Suma
+    {
+        int n1 { get; set; }
+
+        int n2 { get; set; }
+
+
+        public Suma(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+
+        public void sumar()
+        {
+            Console.WriteLine(n1 + n2);
+        }
+
+    }
+}
+
+
